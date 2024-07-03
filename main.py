57b0dca35cbbdd8230283b343816285d286412ec
asiripr <asiripramodaya2000@gmail.com>
2024-07-03 14:12:36 +0530

    completed the functions

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def showBalance():
+def showBalance(balance):
     print(f"Your Balance is ${balance:.2f}")
-def withdraw():
+def withdraw(balance):
     amount = float(input("Enter an amount to withdraw from your account: "))
     if amount>balance:
         print("Insufficient balance in your account")
@@ -18,28 +18,31 @@ def deposit():
         return 0
     else:
         return amount
+def main():
+    balance = 0
+    isRunning = True
 
-balance = 0
-isRunning = True
+    while isRunning:
+        print("---Banking Program---")
+        print("1. Show Balance")
+        print("2. Withdraw")
+        print("3. Deposit")
+        print("4. Exit")
 
-while isRunning:
-    print("---Banking Program---")
-    print("1. Show Balance")
-    print("2. Withdraw")
-    print("3. Deposit")
-    print("4. Exit")
+        choice = input("Enter your choice (1-4): ")
 
-    choice = input("Enter your choice (1-4): ")
+        if choice == '1':
+            showBalance(balance)
+        elif choice == '2':
+            balance-=withdraw(balance)
+        elif choice == '3':
+            balance+=deposit()
+        elif choice == '4':
+            isRunning = False
+        else:
+            print("Your input is not valid.")
 
-    if choice == '1':
-        showBalance()
-    elif choice == '2':
-        balance-=withdraw()
-    elif choice == '3':
-        balance+=deposit()
-    elif choice == '4':
-        isRunning = False
-    else:
-        print("Your input is not valid.")
+    print("Thank you for banking with us. Have a nice day!")
 
-print("Thank you for banking with us. Have a nice day!")
+if __name__ == '__main__':
+    main()
